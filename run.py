3c547bfa21571e75c8efbed2f58a2134205d8a0a
Victor Lopes <72871157+VictorL0pes@users.noreply.github.com>
2025-03-01 22:20:13 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,21 @@
-from app import app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from routes.main import init_routes
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    db.init_app(app)
+    init_routes(app)
+    
+    with app.app_context():
+        db.create_all()
+    
+    return app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app = create_app()
+    app.run(debug=True, host='127.0.0.1', port=5001)
